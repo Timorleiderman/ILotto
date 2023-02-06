a041ca1154c0b7bd3c88915782aab5ad33069c01
Timor <timorleiderman@gmail.com>
2023-02-06 19:42:00 +0200

    Timor: updated train

--- train.py
+++ train.py
@@ -1,10 +1,24 @@
 import os
 import pandas as pd
 import tensorflow as tf
+import matplotlib.pyplot as plt
 from tensorflow.keras import callbacks
 from ilotto import ILotto, CosineAnnealingScheduler
 
 
+def plot_results(hist):
+    print(hist['val_sparse_top_k'].max())
+
+    plt.figure(figsize = (8, 6))
+    plt.semilogy(hist['sparse_top_k'], '-r', label = 'Training')
+    plt.semilogy(hist['val_sparse_top_k'], '-b', label = 'Validation')
+    plt.ylabel('Sparse Top K Accuracy', fontsize = 14)
+    plt.xlabel('Epochs', fontsize = 14)
+    plt.legend(fontsize = 14)
+    plt.grid()
+    plt.show()
+    
+    
 def train(X_train, y_train, X_test, y_test, epochs=200, batch_size=32, lr_max=1e-4, lr_min=1e-6, checkpoint_path="training_2/cp-{epoch:04d}.ckpt"):
     
     model = ILotto()  
@@ -40,6 +54,16 @@ if __name__ == "__main__":
     
     lotto_ds = fetch_dataset()
     X_train, y_train, X_test, y_test = train_test_split(lotto_ds)
+    print(lotto_ds.shape)
+    print(X_train.shape)
+    print(y_train.shape)
+    print(X_test.shape)
+    print(y_test.shape)
+    
     checkpoint_path="training_0/cp-{epoch:04d}.ckpt"
     hist = train(X_train, y_train, X_test, y_test, epochs=200, batch_size=32, lr_max=1e-4, lr_min=1e-6, checkpoint_path=checkpoint_path)
     evaluate(checkpoint_path, X_test, y_test)
+    
+    
+
+    
