5b3f12f2ac754c5150c67b5320733b0cd7dede03
Timor <Timorleiderman@gmail.com>
2025-03-28 23:19:09 +0300

    Update ilotto.py

--- ilotto.py
+++ ilotto.py
@@ -8,36 +8,6 @@ from tensorflow.keras.layers import SpatialDropout1D, Lambda, Bidirectional, con
 from tensorflow.keras.layers import AdditiveAttention, Attention
 
 
-class CosineAnnealingScheduler(callbacks.Callback):
-    """Cosine annealing scheduler."""
-
-    def __init__(self, T_max, eta_max, eta_min=0, verbose=0):
-        super(CosineAnnealingScheduler, self).__init__()
-        self.T_max = T_max
-        self.eta_max = eta_max
-        self.eta_min = eta_min
-        self.verbose = verbose
-
-    def on_epoch_begin(self, epoch, logs=None):
-        if not hasattr(self.model.optimizer, "learning_rate"):
-            raise ValueError('Optimizer must have a "learning_rate" attribute.')
-        learning_rate = (
-            self.eta_min
-            + (self.eta_max - self.eta_min)
-            * (1 + math.cos(math.pi * epoch / self.T_max))
-            / 2
-        )
-        backend.set_value(self.model.optimizer.learning_rate, float(learning_rate))
-        if self.verbose > 0:
-            print(
-                "\nEpoch %05d: CosineAnnealingScheduler setting learning "
-                "rate to %s." % (epoch + 1, learning_rate)
-            )
-
-    def on_epoch_end(self, epoch, logs=None):
-        logs = logs or {}
-        logs["learning_rate"] = backend.get_value(self.model.optimizer.learning_rate)
-
 @saving.register_keras_serializable(package="ILotto")
 class ILotto(tf.keras.Model):
     def __init__(
